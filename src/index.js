c62b9bde7155fa09da58a87ec50702f2ead546e3
Robin HILAIRE <hilairerob84@gmail.com>
2026-09-02 20:07:00 +0200

    Compute the copyright year at load time and credit the Paris open data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './stylesheets/styles.scss'
 
 function start()
 {
+    // L'annee du copyright est calculee au chargement plutot que figee dans le
+    // gabarit : une annee en dur devient fausse le 1er janvier suivant.
+    const annee = document.getElementById('annee')
+    if (annee) annee.textContent = new Date().getFullYear()
+
     let app = new App()
 }
 
